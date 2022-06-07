67835514294ad89bfc9777ef62609ea7548f96ce
Luciano Gonzalez <lucianogonzalez@Air-de-Luciano.fibertel.com.ar>
2022-06-06 23:54:43 -0300

    Ãclase 4 gonzalez

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
+import ItemCount from './components/ItemCount';
 import ItemListContainer from './components/ItemListContainer';
 import NavBar from './components/NavBar';
 
 
 function App() {
+  const onAdd = (count) => {
+   alert(`sumaste ${count} productos`);
+  }
   return (
   <>
   <NavBar />
   <ItemListContainer saludo={'Hola te doy la bienvenida a mi pagina'}/>
+  <ItemCount inicial={1} max={10} onAdd={onAdd}/>
   </>
   );
 }
---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,35 @@
+import  React, { useState } from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+ function ItemCount({inicial, max, onAdd}) {
+     const [count, setCount] = useState(inicial);
+
+     const sumar = () => {
+         count < max ? setCount(count + 1) :  alert('No podes agregar mas productos');   
+     }
+     const restar = () => {
+         count > inicial ? setCount(count - 1) : alert('No puedes quitar mas productos');
+     }
+     const reset = () => {
+       setCount(inicial);
+     }
+
+  return (
+  <>
+    <h2 className='h1Saludo'>{count}</h2>
+      <div className='contador'>
+    <Stack direction="row" spacing={2}> 
+      <Button onClick={sumar} variant="contained" style={{background:'black', color:'white'}}>
+        +
+      </Button>
+      <Button onClick={restar} variant="outlined" style={{background:'black', color:'white'}}>
+       -
+      </Button>
+      <Button onClick={() => { onAdd(count); reset()}} style={{background:'black', color:'white'}}>Agregar al carrito</Button>
+    </Stack>
+    </div>
+    </>
+  );
+}
+export default ItemCount;
